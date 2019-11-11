4c94579376b575161be07964f5a406ba03c379b7
Robert Sipek <sipek@mzk.cz>
2019-11-11 10:50:09 +0100

    Update section osobnosti

--- files_src/script/page.js
+++ files_src/script/page.js
@@ -18,7 +18,7 @@ $.fn.pplSlider = function (e) {
             config.container = '#' + ID;
             tns(config);
         }
-
+        
         that.find('.tns-outer .ppl-list .item').click(function (e) {
             var next = that.find('#' + $(this).data('target')).addClass('active');
             console.log(next);
@@ -31,19 +31,19 @@ $.fn.pplSlider = function (e) {
 };
 
 jQuery(document).ready(function () {
-
+    
     var disableRightClick = function (e) {
         e.preventDefault();
     };
-
+    
     $('.ppl.good, .ppl.dark').pplSlider();
-
+    
     $('.periodicals .item .cover').on('contextmenu', disableRightClick).on('click', function () {
         var gallery = $(this).find('img').data('gallery');
         $.fancybox();
         $.fancybox.open(gallery);
     });
-
+    
     // photogallery fancybox
     $("a.fancybox").attr('rel', 'photogallery').fancybox();
 });
